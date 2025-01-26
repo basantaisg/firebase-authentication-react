40a63cac2484206924642e39dadc80da4c87425e
=basantaisg <basantaparajuli.11@gmail.com>
2025-01-26 19:36:28 +0545

    home-setup

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,33 @@
+import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
-  return <div>Home</div>;
+  console.log(getAuth());
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const logOut = async () => {
+    await auth.signOut();
+    navigate('/');
+  };
+  return (
+    <>
+      <div className='container text-center'>
+        <h1>Welcome To HomePage...</h1>
+
+        <div className='my-5'>
+          <h1>{auth.currentUser.displayName}</h1>
+          <h2>{auth.currentUser.email}</h2>
+          <img src={auth.currentUser.photoURL} alt='' />
+        </div>
+
+        <button className='btn btn-warning' onClick={logOut}>
+          LogOut
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
